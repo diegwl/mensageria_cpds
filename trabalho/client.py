5fad97c8ac01264d41599d7d063638b8aa3fe95b
diegwl <diegocastanlopes0210@outlook.com>
2025-04-18 18:50:07 -0300

    código do cliente

---
+++ trabalho/client.py
@@ -0,0 +1,35 @@
+
+import socket
+import threading
+
+HOST = 'localhost'
+PORT = 5555
+
+def receive(sock):
+    while True:
+        try:
+            data = sock.recv(1024).decode()
+            if data:
+                print(data)
+        except:
+            break
+
+def main():
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.connect((HOST, PORT))
+
+        name_prompt = s.recv(1024).decode()
+        print(name_prompt)
+        name = input("> ")
+        s.sendall(name.encode())
+
+        threading.Thread(target=receive, args=(s,), daemon=True).start()
+
+        while True:
+            msg = input()
+            if msg.lower() == "sair":
+                break
+            s.sendall(msg.encode())
+
+if __name__ == "__main__":
+    main()
